20ffb13130be71910d25848f334228409b927c07
mohammad reza faraji <realrezafarajiofficial@gmail.com>
2025-03-06 22:19:35 +0330

    Add files via upload

---
+++ Tamrin2.py
@@ -0,0 +1,20 @@
+import math
+
+print('baraye mohasebe rishe hay yek moadele daraje 2 be sorat "ax^2+bx=c=0" zarayeb khasye shode ra vared konid! \n')
+a = int(input('magdar "a" ra vared konid :'))
+b = int(input('magdar "b" ra vared konid :'))
+c = int(input('magdar "c" ra vared konid :'))
+delta = b**2-(4*a*c)
+radikal_delta = math.sqrt(delta)
+
+if delta > 0:
+    x1 = (-b+radikal_delta)/(2*a)
+    x2 = (-b-radikal_delta)/(2*a)
+    print('in moadele do rishe hagigi %f va %f darad.' % (x1, x2))
+
+elif delta == 0:
+    x1 = (-b)/(2*a)
+    print('in moadele yak rishe hagigi %f darad.' % x1)
+
+else:
+    print('in moadele rishe hagigi nadarad')
---
+++ Tamrin3.py
@@ -0,0 +1,3 @@
+a = int(input('yek adad vared konid : '))
+for i in range(0, a+1):
+    print('tavane 2 adad %d = %d' % (i, i**2))
