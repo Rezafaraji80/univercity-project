093d9ed54907ef749f38a3580c46d2ed22f0d99e
mohammad reza faraji <realrezafarajiofficial@gmail.com>
2025-03-06 22:51:18 +0330

    Tamrin2.py

--- Tamrin2.py
+++ Tamrin2.py
@@ -4,10 +4,11 @@ print('baraye mohasebe rishe hay yek moadele daraje 2 be sorat "ax^2+bx=c=0" zar
 a = int(input('magdar "a" ra vared konid :'))
 b = int(input('magdar "b" ra vared konid :'))
 c = int(input('magdar "c" ra vared konid :'))
+print('moadele barabar ba %dX^2 + %dX + %d = 0 ast.' % (a, b, c))
 delta = b**2-(4*a*c)
-radikal_delta = math.sqrt(delta)
 
 if delta > 0:
+    radikal_delta = math.sqrt(delta)
     x1 = (-b+radikal_delta)/(2*a)
     x2 = (-b-radikal_delta)/(2*a)
     print('in moadele do rishe hagigi %f va %f darad.' % (x1, x2))
